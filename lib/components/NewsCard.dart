d5db139821f95082225b5d2aca3ab8450469af64
Vitaly Posadnev <elemabor@gmail.com>
2021-04-10 00:34:54 +0300

    update news card (added select news method)

--- lib/components/NewsCard.dart
+++ lib/components/NewsCard.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/models/NewsModel.dart';
-import 'package:flutter_application_1/pages/NewsDetailPage.dart';
+import 'package:provider/provider.dart';
+
+import '../models/NewsModel.dart';
+import '../pages/NewsDetailPage.dart';
+import '../providers/NewsProvider.dart';
 
 class NewsCard extends StatelessWidget {
   final NewsModel news;
@@ -9,63 +12,66 @@ class NewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => NewsDetailPage(
-              newsId: news.id,
+    return Consumer<NewsProvider>(
+      builder: (context, provider, child) {
+        return GestureDetector(
+          onTap: () {
+            provider.updateSelectedNews(news);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => NewsDetailPage(),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                color: Colors.grey,
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  child: Image.network(
+                    news.adt,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    news.title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    news.createdAt,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         );
       },
-      child: Container(
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          border: Border.all(
-            color: Colors.grey,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                border: Border.all(
-                  color: Colors.grey,
-                ),
-              ),
-              child: Image.network(
-                news.adt,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Text(
-                news.title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Container(
-              child: Text(
-                news.createdAt,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
