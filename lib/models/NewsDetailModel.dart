b07c781c8f02284dad60a8f2baf7975609b66bfd
Vitaly Posadnev <elemabor@gmail.com>
2021-04-09 18:58:32 +0300

    Create news detail model

---
+++ lib/models/NewsDetailModel.dart
@@ -0,0 +1,33 @@
+import 'dart:convert' as c;
+
+class NewsDetailModel {
+  int id;
+  String createdAt;
+  String title;
+  String content;
+  String adt;
+  List<String> images;
+
+  NewsDetailModel.fromJson(Map<String, dynamic> json) {
+    images = [];
+    id = json['id'];
+    createdAt = json['created_at'];
+    title = json['title'];
+    content = json['content'];
+    adt = json['adt'];
+    for (var image in json['files']) {
+      images.add(image);
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    var json = {};
+    json['id'] = id;
+    json['created_at'] = createdAt;
+    json['title'] = title;
+    json['content'] = content;
+    json['adt'] = adt;
+    json['files'] = c.jsonEncode(images);
+    return json;
+  }
+}
